9063813b018f9cf52a8e5e684513b49e7d78834c
David Hallberg Jönsson <d@vidhallberg.se>
2019-12-01 18:07:03 +0100

    Redesign

---
+++ dist/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.getElementsByClassName('sectionHeader');
+  const links = document.querySelectorAll('#toc li a');
+
+  const makeActive = link => links[link].classList.add('active');
+
+  const removeActive = link => links[link].classList.remove('active');
+
+  const removeAllActive = () => [...Array(sections.length).keys()].forEach(link => removeActive(link));
+
+  let currentActive = -1;
+  const sectionMargin = 200;
+  window.addEventListener('scroll', () => {
+    const current = sections.length - [...sections].reverse().findIndex(s => window.scrollY >= s.offsetTop - sectionMargin) - 1;
+
+    if (current !== currentActive) {
+      removeAllActive();
+      currentActive = current;
+      makeActive(current);
+    }
+  });
+});
